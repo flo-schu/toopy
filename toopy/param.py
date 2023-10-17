e7af653cb815c0321ad880350b4aab31ced618c1
flo-schu <fluncki@protonmail.com>
2023-10-17 11:56:52 +0200

    Create config module from timepath helpers

--- toopy/param.py
+++ toopy/param.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Param:
     def __init__(self, value, name="", min=None, max=None, step=None, 
@@ -5,11 +6,37 @@ class Param:
             self.name = name
             self._type = type(value)
             self._type.__init__(value)
-            self.min = self._type(value / 4 if min is None else min)
-            self.max = self._type(value * 2 if max is None else max)
+            self.min = self._type(self.minfunc(value, min))
+            self.max = self._type(self.maxfunc(value, min))
             self.step = self._type(1 if step is None else step)
             self.prior = prior
 
+    def minfunc(self, value, minimum):
+        if minimum is not None:
+            return minimum
+        
+        if value <= 1:
+            minimum = value / 10
+        else:
+            minimum = value / 4
+    
+        return np.round(maximum, np.abs(np.min([0, self.scale])))
+        
+    def maxfunc(self, value, maximum):
+        if maximum is not None:
+            return maximum
+        
+        if value <= 1:
+            maximum = value * 10
+        else:
+            maximum = value * 2
+            
+        return np.round(maximum, np.abs(np.min([0, self.scale])))
+    
+    @property
+    def scale(self):
+        return self._type(np.log10(np.abs(self)))
+        
     def ParamClass(self, value):
         kwargs = self.__dict__.copy()
         _type = kwargs.pop("_type")
